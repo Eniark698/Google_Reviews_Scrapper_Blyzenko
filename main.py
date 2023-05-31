73eaddb1bc0f629a3561fa61e23f454478897473
Nazarii Mozol <nmozol@berta.com.ua>
2023-05-31 13:09:02 +0300

    v0.5.1
    infinite clicking on button released

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -41,40 +42,51 @@ except Exception:
 """
 # Wait until page is loaded
 time.sleep(5)
-"""
-# Scrolling to load all reviews
-while True:
-   driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-   time.sleep(3)
-   if driver.find_elements(By.CSS_SELECTOR, '.section-loading.noprint'):
-       break
-"""
-# Get page source and parse it
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-driver.close()
-
-# Get all review containers
-containers = soup.find_all('div', class_='DsOcnf')
 
 # Define list to store reviews data
 reviews = []
+i=0
 
-# Extract data from each container
-for container in containers:
-   adress = container.find('span', class_='ijHgsc').text
-   name = container.find('a', class_='bFubHb').text
-   time = container.find('span', class_='Wxf3Bf wUfJz').text
-   #rating = container.find('span', class_='ODSEW-ShBeI-H1e3jb').get('aria-label')
-   #review_text = container.find('span', class_='DT6Wnd').text
+# Clicking the button to load more reviews
+while True:
+    try:
+        # Find the button and click
+        next_button = driver.find_element(By.XPATH, '//i[contains(text(), "navigate_next")]')
+        driver.execute_script("arguments[0].click();", next_button)
+        time.sleep(3)  # Wait for the reviews to load
+      
 
-   code_filia = container.find('span', class_='mjZtse wjs4p').text
-   i=code_filia.find(':')
-   code_filia=code_filia[i+1:]
+        # Get page source and parse it
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        # Get all review containers
+        containers = soup.find_all('div', class_='DsOcnf')
+        
 
 
-   answer_text = container.find('div', class_='DT6Wnd')
-   print(type(answer_text))
-   reviews.append({'adress':adress, 'name': name, 'time': time, 'code_filia': code_filia, 'answer_text':answer_text})
+        # Extract data from each container
+        for container in containers:
+            adress = container.find('span', class_='ijHgsc').text
+            name = container.find('a', class_='bFubHb').text
+            date_review = container.find('span', class_='Wxf3Bf wUfJz').text
+            #rating = container.find('span', class_='ODSEW-ShBeI-H1e3jb').get('aria-label')
+            #review_text = container.find('span', class_='DT6Wnd').text
+
+            code_filia = container.find('span', class_='mjZtse wjs4p').text
+            i=code_filia.find(':')
+            code_filia=code_filia[i+1:]
+
+
+            answer_text = container.find('div', class_='DT6Wnd')
+            reviews.append({'adress':adress, 'name': name, 'date_review': date_review, 'code_filia': code_filia, 'answer_text':answer_text})
+        break
+    except NoSuchElementException:
+        # If the button is not found on the page, break the loop
+        break
+
+
+driver.close()
+
+
 
 # Create dataframe
 df = pd.DataFrame(reviews)
