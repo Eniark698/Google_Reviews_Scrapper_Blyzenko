f56fc8ea99adb456e3cfcecb887351f783ff54ad
Nazarii Mozol <nmozol@berta.com.ua>
2023-06-01 11:10:57 +0300

    v0.9.1
    convert relative datetime to ablosulte, other fixes

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import xpath
-
+import dateparser
 
 # Your profile path
 profile_path = 'C:/Users/nmozol/AppData/Local/Google/Chrome/User Data/Default'
@@ -47,77 +47,89 @@ time.sleep(5)
 
 # Define list to store reviews data
 reviews = []
-i=0
+j=0
 
 # Clicking the button to load more reviews
 while True:
-    try:
-       
-        # Get page source and parse it
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        # Get all review containers
-        containers = soup.find_all('div', class_='DsOcnf')
+   
+    j+=1
+    # Get page source and parse it
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # Get all review containers
+    containers = soup.find_all('div', class_='DsOcnf')
+    
+
+
+    # Extract data from each container
+    for container in containers:
+        # try:
+        #     # If the "More" button is found, click it
+        #     more_button = container.find('a', jsname_='lOwe6b')
+        #     driver.execute_script("arguments[0].click();", more_button)
+        #     time.sleep(1)  # Wait for the text to expand
+        # except NoSuchElementException:
+        #     # If the "More" button is not found, continue without clicking
+        #     pass
+
+        adress = container.find('span', class_='ijHgsc').text
+        name = container.find('a', class_='bFubHb').text
+        date_review = container.find('span', class_='Wxf3Bf wUfJz').text
+        rating = container.find_all('span', class_='DPvwYc L12a3c z3FsAc')
+        rating=len(rating)
+        
+        try:
+            review_text = container.find('span', class_='oiQd1c').text
+        except:
+            review_text=''
+
+
+        user_url=container.find('a', class_='bFubHb').attrs['href']
+        try:
+            replay_time=container.find('span', class_='Wxf3Bf Gjqk4b').text
+            replay_time=dateparser.parse(replay_time, languages=['uk'])
+            replay_time=replay_time.strftime('%Y-%m-%d')
+        except:
+            replay_time=''
+
+        code_filia = container.find('span', class_='mjZtse wjs4p').text
+        i=code_filia.find(':')
+        code_filia=code_filia[i+1:]
+
+        try:
+            reply_text = container.find('div', class_='DT6Wnd').text
+        except:
+            reply_text=''
         
 
+        date_review=dateparser.parse(date_review, languages=['uk'])
+        date_review=date_review.strftime('%Y-%m-%d')
 
-        # Extract data from each container
-        for container in containers:
-            # try:
-            #     # If the "More" button is found, click it
-            #     more_button = container.find('a', jsname_='lOwe6b')
-            #     driver.execute_script("arguments[0].click();", more_button)
-            #     time.sleep(1)  # Wait for the text to expand
-            # except NoSuchElementException:
-            #     # If the "More" button is not found, continue without clicking
-            #     pass
+        reviews.append({'date_review': date_review
+                        ,'code_filia': code_filia
+                        ,'adress':adress
+                        ,'name': name
+                        ,'user_url':user_url
+                        ,'review_text':review_text
+                        ,'replay_time':replay_time
+                        ,'reply_text':reply_text
+                        ,'rating':rating})
 
-            adress = container.find('span', class_='ijHgsc').text
-            name = container.find('a', class_='bFubHb').text
-            date_review = container.find('span', class_='Wxf3Bf wUfJz').text
-            rating = container.find_all('span', class_='DPvwYc L12a3c z3FsAc')
-            rating=len(rating)
-            
-            try:
-                review_text = container.find('span', class_='oiQd1c').text
-            except:
-                review_text=''
-
-
-            user_url=container.find('a', class_='bFubHb').attrs['href']
-            try:
-                replay_time=container.find('span', class_='Wxf3Bf Gjqk4b').text
-            except:
-                replay_time=''
-
-            code_filia = container.find('span', class_='mjZtse wjs4p').text
-            i=code_filia.find(':')
-            code_filia=code_filia[i+1:]
-
-            try:
-                reply_text = container.find('div', class_='DT6Wnd').text
-            except:
-                reply_text=''
-            
-            reviews.append({'date_review': date_review
-                            ,'code_filia': code_filia
-                            ,'adress':adress
-                            ,'name': name
-                            ,'user_url':user_url
-                            ,'review_text':review_text
-                            ,'replay_time':replay_time
-                            ,'reply_text':reply_text
-                            ,'rating':rating})
-
-
-
-        # Find the button and click
+    # try:
+    #     button=soup.find_all('button', class_='VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ QDwDD mN1ivc vX5N7b').attrs['disabled']
+    #     break
+    # except:
+    #     pass
+    # Find the button and click
+    # if j>=5:
+    #     break
+    try:
         next_button = driver.find_element(By.XPATH, '//i[contains(text(), "navigate_next")]')
         driver.execute_script("arguments[0].click();", next_button)
-        time.sleep(3)  # Wait for the reviews to load
-        
-    except NoSuchElementException:
-        # If the button is not found on the page, break the loop
+    except:
         break
+    time.sleep(3)  # Wait for the reviews to load
+    
+time.sleep(3)
 
 
 driver.close()
