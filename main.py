90c18d9b04bddbb837c348ed6c048d70a4f93330
Nazarii Mozol <nmozol@berta.com.ua>
2023-09-07 09:59:34 +0300

    reworked login
    add autologin, pass, number and manual sms code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -8,7 +10,6 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 import time
-import xpath
 import dateparser
 import datetime
 import pyodbc
@@ -18,21 +19,22 @@ import os
 from warnings import filterwarnings
 filterwarnings("ignore")
 
-
+j=0
 # get the current working directory
 current_working_directory = os.getcwd()
 
 try:
     #import secret variables
     import base64
-    from secrets_file import server,database,port,user,password
+    from secrets_file import server,database,port,user,password,login,passw, number
     server=base64.b64decode(server.decode("utf-8")).decode()
     database=base64.b64decode(database.decode("utf-8")).decode()
     port=base64.b64decode(port.decode("utf-8")).decode()
     user=base64.b64decode(user.decode("utf-8")).decode()
     password=base64.b64decode(password.decode("utf-8")).decode()
-
-
+    login=base64.b64decode(login.decode("utf-8")).decode()
+    passw=base64.b64decode(passw.decode("utf-8")).decode()
+    number=base64.b64decode(number.decode("utf-8")).decode()
 
 
     #connect to sql server
@@ -44,19 +46,24 @@ try:
     #cnxn.setencoding('latin1')
 
     # Your profile path
-    profile_path = 'C:/Users/nmozol/AppData/Local/Google/Chrome/User Data/Profile 1'
+    #profile_path = '/home/administartor/.config/google-chrome/default'
 
     # Setup selenium webdriver with a profile
     options = Options()
-    options.add_argument(f'user-data-dir={profile_path}')
+    #options.add_argument(f'user-data-dir={profile_path}')
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-plugins-discovery")
     options.add_argument("--start-maximized")
-    options.add_argument("user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
+    options.add_argument("--incognito")
+    #options.add_argument("--headless")
+    #options.add_argument("user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
 
-    s=Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s, options=options)
+    #s=Service(ChromeDriverManager().install())
+    
+
+    #driver = webdriver.Chrome(service=s, options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
 
 
 
@@ -66,23 +73,106 @@ try:
     # Target URL
     url = 'https://business.google.com/u/0/groups/113213648489666118794/reviews'  # Fill the URL of the Google Reviews
     driver.get(url)
-
+    # driver.find_element(By.XPATH , '/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input').send_keys(login)
+    # driver.find_element(By.CLASS_NAME , 'VfPpkd-vQzf8d').click()
+    # time.sleep(10)
+    # driver.find_element(By.CLASS_NAME , 'pp').sendKeys(passw)
+    # driver.find_element(By.XPATH , 'VfPpkd-vQzf8d').click()
 
     # # Accept the cookies
-    # time.sleep(3)
+    time.sleep(3)
     # try:
     #     driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span').click()
     # except Exception:
     #    pass
 
+    try:
+        # Enter login
+        email_input = driver.find_element(By.XPATH, '//*[@type="email"]')
+        email_input.send_keys(login)
+        next_button = driver.find_element(By.XPATH, '//*[@id="identifierNext"]')
+        next_button.click()
+
+        # Wait for the password input to become available
+        time.sleep(10)
+
+        # Enter password
+        password_input = driver.find_element(By.XPATH, '//*[@type="password"]')
+        password_input.send_keys(passw)
+        next_button = driver.find_element(By.XPATH, '//*[@id="passwordNext"]')
+        next_button.click()
+
+
+
+
+        # Assuming at this point the 2-factor authentication might kick in.
+        # Wait for a short while for it to be presented, or any other form of 2FA your account has set up
+        time.sleep(10)
+
+        # Check if phone number input is present
+        try:
+
+
+            try:
+                # Locate the checkbox using its XPath (you would need to find the exact XPath from the browser's developer tools)
+                checkbox = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[3]/div[1]/div/div/div[1]/div/input')))
+
+                # Check the state of the checkbox
+                if not checkbox.is_selected():
+                    # If the checkbox is not selected, select it
+                    checkbox.click()
+            except:
+                # Handle the exception (e.g., checkbox not found)
+                print("Checkbox not found")
+
+            phone_number_input = driver.find_element(By.XPATH, '//*[@type="tel"]') # Adjust the XPath based on the actual element
+            phone_number_input.send_keys(number)
+            next_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button') # Adjust the XPath based on the actual button
+            next_button.click()
+            
+            # Now wait for the code input
+            time.sleep(10)
+
+            try:
+                # Locate the checkbox using its XPath (you would need to find the exact XPath from the browser's developer tools)
+                checkbox = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[3]/div[1]/div/div/div[1]/div/input')))
+
+                # Check the state of the checkbox
+                if not checkbox.is_selected():
+                    # If the checkbox is not selected, select it
+                    checkbox.click()
+            except:
+                # Handle the exception (e.g., checkbox not found)
+                print("Checkbox not found")
+
+            two_factor_input = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[2]/div/div[1]/div/div[1]/input') # Adjust the XPath based on the actual 2FA element
+            two_factor_code = input("Please enter your 2-factor authentication code from SMS: ")
+            two_factor_input.send_keys(two_factor_code)
+            next_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button') # Adjust the XPath based on the actual button
+            next_button.click()
+        except Exception as e:
+            print(f"2FA input not found or another error occurred: {str(e)}")
+    except:
+        f=open(current_working_directory + '/log.txt', 'a')
+        f.write('----------------------------------------\n')
+        f.write('no logging necessary\n')
+        f.write('occurred on ' + str(datetime.datetime.now())+ '\n')
+        f.write('----------------------------------------\n\n\n')
+        f.close()
+    else:
+        f=open(current_working_directory + '/log.txt', 'a')
+        f.write('----------------------------------------\n')
+        f.write('login\n')
+        f.write('occurred on ' + str(datetime.datetime.now())+ '\n')
+        f.write('----------------------------------------\n\n\n')
+        f.close()
     # Wait until page is loaded
-    time.sleep(5)
+    time.sleep(40)
 
     # Define list to store reviews data
 
     #reviews = []
-    j=0
-
+  
 
     #clearing table
     cursor.execute("delete from {}.{}.[dbo].[Google_Review_Scrapping];".format(server,database))
@@ -210,9 +300,9 @@ try:
     # Save to csv
     #df.to_csv('./reviews.csv', index=False, sep='\t', encoding='utf-16')
 
-    print('Reviews are saved.')
+    #print('Reviews are saved.')
 except:
-        f=open(current_working_directory + '/log.txt', 'a')
+        f=open(current_working_directory + '/log_err.txt', 'a')
         f.write('----------------------------------------\n')
         f.write(format_exc())
         f.write('\nstopped on : '+ str(j) + '  th page\n')
