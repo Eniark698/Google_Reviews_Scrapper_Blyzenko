830ef7aed54ac7e1a073d63dca84fbb299944a10
Nazarii Mozol <nmozol@berta.com.ua>
2023-10-05 12:17:44 +0300

    fix

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ try:
                 # Handle the exception (e.g., checkbox not found)
                 print("Checkbox not found")
 
-            phone_number_input = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[2]/div/div[2]/div[1]/label/input') # Adjust the XPath based on the actual element
+            phone_number_input = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section[3]/div/div/div[2]/div/div[2]/div[1]/label/input') # Adjust the XPath based on the actual element
             phone_number_input.send_keys(number)
             next_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button') # Adjust the XPath based on the actual button
             next_button.click()
