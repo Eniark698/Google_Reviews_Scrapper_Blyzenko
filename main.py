d919aff235d43b2c8efe9b947cf86ee48e21f4f6
Nazarii Mozol <nmozol@berta.com.ua>
2023-10-05 12:09:53 +0300

    microfix with chrome

--- main.py
+++ main.py
@@ -56,6 +56,8 @@ try:
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-plugins-discovery")
     options.add_argument("--start-maximized")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     #options.add_argument("--incognito")
     #options.add_argument("--headless")
     options.add_argument("user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
